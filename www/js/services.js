65825d55da776b1500ea7023b9b2c80673cad9ad
mramadhant <muh.ramadhan96@gmail.com>
2016-05-12 18:08:56 +0700

    first commit

---
+++ www/js/services.js
@@ -0,0 +1,37 @@
+angular.module('starter.services', [])
+
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Andi',
+    face: 'img/ben.png'
+  }, {
+    id: 1,
+    name: 'Bambang',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Joko',
+    face: 'img/adam.jpg'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+});
